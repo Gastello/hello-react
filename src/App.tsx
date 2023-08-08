3e0afd0045977fa815bcbfe358915b6244acb207
Gastello <maximgst@gmail.com>
2023-08-08 18:47:47 +0300

    com

--- src/App.tsx
+++ src/App.tsx
@@ -7,36 +7,77 @@ import { JSX } from "react/jsx-runtime";
 
 const reactName = "React";
 
+type H1Props = {
+  text: string;
+  isCursive: boolean;
+};
+class H1 extends PureComponent<H1Props> {
+  render() {
+    return this.props.isCursive ? (
+      <h1>
+        <i>{this.props.text}</i>
+      </h1>
+    ) : (
+      <h1>{this.props.text}</h1>
+    );
+  }
+}
+
+type H2Props = {
+  text: string,
+}
+const H2: FC<H2Props> = (props) => {
+  return (
+    <h2>{props.text}</h2>
+  )
+}
+
+type PProps = {
+  text: string,
+}
+class P extends PureComponent<PProps>{
+  render(){
+    return (
+      <p>{this.props.text}</p>
+    )
+  }
+}
+
+type ImgProps = {
+  src: string,
+  className: string,
+  alt: string,
+}
+const Img: FC<ImgProps> = (props) => {
+  return (
+    <img src={props.src} className={props.className} alt={props.alt} />
+  )
+}
+
 const NewbieInfo = () => (
   <>
-    <img src={logo} className="App-logo" alt="logo" />
-    <h1>{"Hello, " + reactName + "!"}</h1>
-    <h1>
-      <i>JavaScript - бібліотека для створення користувацьких інтерфейсів</i>
-    </h1>
-    <h2>Створення проєкту</h2>
-    <p>npx create-react-app myproj --template typescript</p>
-    <h2>Структура проєкту</h2>
-    <img src={require("./images/1.png")} alt="" />
-    <h2>Запуск проєкту для локальної розробки</h2>
-    <p>npm start</p>
-    <h2>Запуск тестів проєкту</h2>
-    <p>npm test</p>
-    <h2>Запуск проєкту для production</h2>
-    <p>npm run build</p>
-    <p>
-      При застосуванні цієї команди проєкт збирається в один файл та
-      мініфікується.
-    </p>
-    <h2>Конвертація create-react-app проєкту в звичайний react</h2>
-    <p>npm run eject</p>
-    <p>
-      Якщо потрібно налаштувати проєкт з нуля під себе, то використовується ця
-      команда.
-    </p>
-    <img src={require("./images/2.png")} alt="" />
-    <h2>Використання змінних у коді</h2>
-    <img src={require("./images/3.png")} alt="" />
+    <Img src={logo} className="App-logo" alt="logo"/> 
+    <H1 text={"Hello, " + reactName + "!"} isCursive={false} />
+    <H1 text="JavaScript - бібліотека для створення користувацьких інтерфейсів" isCursive={true}></H1>
+    <H2 text="Створення проєкту" />
+    <P text="npx create-react-app myproj --template typescript"/> 
+    <H2 text="Структура проєкту" /> 
+    <Img src={require("./images/1.png")} className="" alt=""/>
+    <H2 text="Запуск проєкту для локальної розробки" />  
+    <P text="npm start"/>  
+    <H2 text="Запуск тестів проєкту" />  
+    <P text="npm test"/>  
+    <H2 text="Запуск проєкту для production" />   
+    <P text="npm run build"/>  
+    <P text="При застосуванні цієї команди проєкт збирається в один файл та
+      мініфікується."/>  
+    <H2 text="Конвертація create-react-app проєкту в звичайний react" />  
+    <P text="npm run eject"/>  
+    <P text="Якщо потрібно налаштувати проєкт з нуля під себе, то використовується ця
+      команда."/>   
+    <Img src={require("./images/2.png")} className="" alt=""/>
+    <H2 text="Використання змінних у коді" />   
+    <Img src={require("./images/3.png")} className="" alt=""/>
   </>
 );
 
@@ -124,7 +165,7 @@ type CallbackProps = {
 
 const CallbackFC: FC<CallbackProps> = (props) => {
   return (
-    <button onClick={()=>props.doSomething("some magic power.")}>
+    <button onClick={() => props.doSomething("some magic power.")}>
       Click on me to feel the power of getting data from component!
     </button>
   );
@@ -133,21 +174,21 @@ const CallbackFC: FC<CallbackProps> = (props) => {
 const App = () => (
   <div className="App">
     <NewbieInfo />
-    <h1>Компоненти</h1>
-    <Timer />
-    <h1>Хуки</h1>
-    <BtnWithCounter />
-    <h1>Передача аргументів у компонент</h1>
-    <h2>Props</h2>
+    <H1 text="Компоненти" isCursive={false}></H1> 
+    <Timer /> 
+    <H1 text="Хуки" isCursive={false}></H1> 
+    <BtnWithCounter /> 
+    <H1 text="Передача аргументів у компонент" isCursive={false}></H1> 
+    <H2 text="Props" />  
     <Circle color="red" radius={200} opacity={0.4}></Circle>
     <Circle color="yellow" radius={200} opacity={0.4}></Circle>
     <Circle color="green" radius={200} opacity={1}></Circle>
-    <Line color="grey" width="70%" height="5px"></Line>
-    <h2>Передача компонента як аргумент</h2>
+    <Line color="grey" width="70%" height="5px"></Line> 
+    <H2 text="Передача компонента як аргумент" />   
     <Frame>I am text in the center!</Frame>
-    <h1>Отримання даних з компоненту</h1>
-    <h2>Callback – найкращий варіант</h2>
-    <CallbackFC doSomething={(text)=>alert(text)}/>
+    <H1 text="Отримання даних з компоненту" isCursive={false}></H1>
+    <H2 text="Callback – найкращий варіант"/>
+    <CallbackFC doSomething={(text) => alert(text)} />
     <Footer copyright="Created by Gastello" />
   </div>
 );
